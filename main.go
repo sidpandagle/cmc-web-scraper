73adbac950ab41b64869188e70dc199db5f702f8
fudge-fantastic <siddhant.pandagle1998@gmail.com>
2024-10-30 00:50:24 +0530

    Initial Init

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"cmc-web-scraper/api/routes"
+	"cmc-web-scraper/pkg/book"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
+	db, cancel, err := databaseConnection()
+	if err != nil {
+		log.Fatal("Database Connection Error $s", err)
+	}
+	fmt.Println("Database connection success!")
+	bookCollection := db.Collection("books")
+	bookRepo := book.NewRepo(bookCollection)
+	bookService := book.NewService(bookRepo)
+
+	app := fiber.New()
+	app.Use(cors.New())
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.Send([]byte("Welcome to the cmc-web-scraper mongo book shop!"))
+	})
+	api := app.Group("/api")
+	routes.BookRouter(api, bookService)
+	defer cancel()
+	log.Fatal(app.Listen(":8080"))
+}
+
+func databaseConnection() (*mongo.Database, context.CancelFunc, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("DB_URI")).SetServerSelectionTimeout(5*time.
+		Second))
+	if err != nil {
+		cancel()
+		return nil, nil, err
+	}
+	db := client.Database(os.Getenv("DB_NAME"))
+	return db, cancel, nil
+}
